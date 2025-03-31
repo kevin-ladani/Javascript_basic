009bf1c5ead32f685e606bf208a9a1e64def9e8d
Kevin Ladani <kevinladani07@gmail.com>
2025-03-31 19:06:18 +0530

    Add files via upload

---
+++ frist.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+const accountId = 1234567890; //cannot be changed
+let accountBalance = 1000.50; //can be changed
+cityname = "New York";
+let phone = 1234567890;
+
+console.table([accountId, accountBalance, cityname, phone]);
+
+//Data Types
+// 1. String
+let name = "kevin";
+// 2. Number
+let age = 25;
+// 3. Boolean
+let isadult = true;
+// 4. Undefined
+let address; //undefined
+// 5. Null
+let phoneNumber = null; 
+
+console.log(name, age, isadult, address, phoneNumber);
