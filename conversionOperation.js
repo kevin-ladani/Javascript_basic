970f97a8a74f914c9c27203b3ebdb3008ee618b8
Kevin Ladani <kevinladani07@gmail.com>
2025-04-01 10:42:48 +0530

    Add files via upload

---
+++ conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "100abc"
+ 
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinNumber = Number(score)
+
+console.log(valueinNumber);
+
+//"100"=>100
+//"100abc"=>NaN   
+let isLogedIn = 0;
+ 
+let isLogedInBoolean = Boolean(isLogedIn)
+
+console.log(isLogedInBoolean); 
+
+let someNumber = 1234.5678
+
+//let someNumberToString = String(someNumber)
+
+console.log(typeof someNumber);
